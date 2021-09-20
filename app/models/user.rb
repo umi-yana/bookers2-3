e740dea550e92e379dda76b1d36d9713ee98cc5e
EC2 Default User <ec2-user@ip-172-31-19-217.ec2.internal>
2021-09-20 09:11:00 +0000

    アソシエーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  has_many :favorite, dependent: :destroy
+  
   attachment :profile_image, destroy: false
 
  validates :name,presence: true, uniqueness: true, length: { in: 2..20 }
